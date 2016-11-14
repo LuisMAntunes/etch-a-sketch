d6e51b1f7a1494e2cb72c7fc265b46ee021374fa
LuisMAntunes <luismiguelsps@gmail.com>
2016-11-14 02:13:28 +0000

    add button to change number of squares

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,38 @@
+var squares = 16;
+
+
+numberOfSquares = function(){
+	var size = prompt("How many squares?")
+	return size;
+}
+
+createGrid = function(squares){
+	$('button').after('<div class="container">');
+		for (var i = 0; i < squares; i++) {
+	 		$('.container').append('<div class="row">');
+	 	};
+		for (var j = 0; j < squares; j++) {
+			$('.row').append('<div class="collumn">')
+		};
+		$('.collumn').height($('.container').height()/squares);
+		$('.collumn').width($('.container').width()/squares);
+		$('.collumn').on('mouseenter', function() {
+			/* Act on the event */
+			$(this).css({'background-color':'black'
+						});
+
+	
+		});
+}
+
+
 $(document).ready(function(){
-	$('body').append('<div class="container">');
-	for (var i = 0; i < 16; i++) {
-	 	$('.container').append('<div class="row">');
-	 };
-	for (var j = 0; j < 16; j++) {
-		$('.row').append('<div class="collumn">')
-	};
-	$('.collumn').on('mouseenter', function() {
-		/* Act on the event */
-		$(this).css({'background-color':'black'});
+	createGrid(squares);
+	$("button").on('click', function() {
+		var squares = numberOfSquares();
+		$('button').nextAll('div').remove();
+		createGrid(squares);
 	});
+
 });
+
